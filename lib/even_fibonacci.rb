d1d495f660585ba0ae869c385aadf9d650fb84a4
gregmarquet <marquet.greg@gmail.com>
2016-11-19 17:15:30 -0500

    Done

--- lib/even_fibonacci.rb
+++ lib/even_fibonacci.rb
@@ -1,1 +1,10 @@
-# Implement your procedural solution here!
+def even_fibonacci_sum(limit) 
+  first, last, sum = 1, 2, 0
+  while last < limit 
+    sum += last if last.even?
+    first, last = last, first + last
+  end
+  sum
+end
+
+  
--- lib/oo_even_fibonacci.rb
+++ lib/oo_even_fibonacci.rb
@@ -1,1 +1,17 @@
-# Implement your object-oriented solution here!
+class EvenFibonacci
+  
+  def initialize(limit)
+    @limit = limit
+  end
+
+  def sum
+    first, last, sum = 1, 2, 0
+    while last < @limit 
+      sum += last if last.even?
+      first, last = last, first + last
+    end
+    sum
+  end
+
+
+end
